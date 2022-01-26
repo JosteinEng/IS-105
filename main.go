1192b554f676822e14d300b98360fa74e8ba7ad2
jostein <josteinen@uia.no>
2022-01-26 15:14:27 +0100

    Jeg har leket med disse quotes, og lagt til alle fire kombinasjoner i en TestQuote string. Når jeg kjører Main.go vil jeg nå få return quote.Hello, Glass, Go og Opt

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(myquote.TestQuote())
+	fmt.Println(myquote.TestQuote1())
 }
--- myquote/myquote.go
+++ myquote/myquote.go
@@ -2,6 +2,6 @@ package myquote
 
 import "rsc.io/quote"
 
-func TestQuote() string {
-	return quote.Glass() + "\n" + quote.Go()
+func TestQuote1() string {
+	return quote.Hello() + "\n" + quote.Glass() + "\n" + quote.Opt() + "\n" + quote.Go()
 }
